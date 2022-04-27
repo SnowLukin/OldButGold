a4735bd619dbec0f8c24dfb56f405775620e7b47
Snow Lukin <everyoursden@gmail.com>
2022-04-28 00:50:35 +0300

    Added V-1 task2

---
+++ V_1/Task2/Task2/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Task2
+//
+//  Created by Snow Lukin on 28.04.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int getSumOfDigits(int number) {
+    int sum = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        sum += number % 10;
+    }
+    return sum;
+}
+
+bool isOdd(int number) {
+    return number % 2 == 0;
+}
+
+int main(int argc, const char * argv[]) {
+    int counter = 0;
+    
+    int number;
+    cin >> number;
+    while (number != 0) {
+        int sumOfDigits = getSumOfDigits(number);
+        if (isOdd(sumOfDigits)) counter++;
+    }
+    cout << counter;
+}
